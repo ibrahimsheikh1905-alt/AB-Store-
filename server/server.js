38f3aa58bb6d025f224ceb1917ae61f6cc41ddc8
ibrahimsheikh1905-alt <ibrahimsheikh1905@gmail.com>
2026-03-31 21:11:25 +0500

    fix server

--- server/server.js
+++ server/server.js
@@ -60,7 +60,7 @@ const connectDB = async () => {
 
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
